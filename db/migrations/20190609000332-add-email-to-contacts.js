021ae989b93ac325115c9d3bedf302ac2d33da1e
Adrienne <adriennenking@gmail.com>
2019-06-08 19:33:14 -0700

    Creating contacts assignment

--- db/migrations/20190609000332-add-email-to-contacts.js
+++ db/migrations/20190609000332-add-email-to-contacts.js
@@ -1,17 +1,15 @@
 'use strict';
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Contacts', {
+    return queryInterface.addColumn('Contacts', 'email' {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
-        type: Sequelize.STRING
-      },
-      phone: {
+      email: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -25,6 +23,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Contacts');
+    return queryInterface.removeColumn('Contacts', 'email');
   }
 };
