b970ed839fd9bc072699b0a3c7d7c6429f6e572a
Adrienne <adriennenking@gmail.com>
2019-06-11 18:46:12 -0700

    Checkpoint work

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -30,6 +30,36 @@ module.exports = class ContactController {
         }
       }
     ];
+    this.searchQuestions = [
+     {
+       type: "input",
+       name: "name",
+       message: "Name of contact to search - ",
+       validate(val){
+         return val !== "";
+       }
+     }
+   ];
+   this.showContactQuestions = [
+      {
+        type: "list",
+        name: "selected",
+        message: "Please choose from an option below: ",
+        choices: [
+          "Delete contact",
+          "Main menu"
+        ]
+      }
+    ];
+
+    this.deleteConfirmQuestions = [
+      {
+        type: "confirm",
+        name: "confirmation",
+        message: "are you sure you want to delete this contact?"
+      }
+    ];
+
   }
 
   addContact(name, phone, email){
@@ -48,10 +78,9 @@ module.exports = class ContactController {
     }
     return null;
   }
-}
 
   binarySearch(contacts, target){
-    let min = 0:
+    let min = 0;
     let max = contacts.length - 1;
     let mid;
 
@@ -70,3 +99,16 @@ module.exports = class ContactController {
 
     return null;
   }
+
+  search(name){
+    return Contact.findOne({
+      where: {name}
+    });
+  }
+
+  delete(id){
+      return Contact.destroy({
+        where: {id}
+      })
+    }
+}
